65c171bdaa8c7a5d17314ff45fffbffdeeadfdf2
Fábio simones <simones.fabio1994@gmail.com>
2024-10-23 14:54:09 -0300

    Criando scripts para banco de dados das Views.

---
+++ Dima.Core/Models/Views/vwGetIncomesByCategory.sql
@@ -0,0 +1,10 @@
+﻿CREATE or ALTER VIEW [vwGetIncomesByCategory] AS
+SELECT [Transaction].[UserId], [Category].[Title] as [Category], Year([Transaction].[PaidOrReceivedAt]) as [Year], SUM([Transaction].[Amount]) as [Expenses]
+from [Transaction] INNER JOIN [Category] ON [Transaction].[CategoryId] = [Category].[Id]
+WHERE[Transaction].[PaidOrReceivedAt] >= DATEADD(Month, -11, Cast(GetDate() as Date))
+AND [Transaction].[PaidOrReceivedAt] < DATEADD(Month, 1, Cast(GetDate() as Date))
+AND [Transaction].[Type] = 1
+GROUP BY
+[Transaction].[UserId],
+[Category].[Title],
+Year([Transaction].[PaidOrReceivedAt])
