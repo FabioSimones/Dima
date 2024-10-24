c0e03282e7031d0034c62f0ed36cca4ea6f0b56e
Fábio simones <simones.fabio1994@gmail.com>
2024-10-24 16:39:25 -0300

    Criação de Handlers e Requests.

---
+++ Dima.Core/Handlers/IReportHandler.cs
@@ -0,0 +1,19 @@
+﻿using Dima.Api.Response;
+using Dima.Core.Models.Reports;
+using Dima.Core.Requests.Reports;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dima.Core.Handlers
+{
+    public interface IReportHandler
+    {
+        Task<Resposta<List<IncomesAndExpenses>?>> GetIncomesAndExpensesReportAsync(GetIncomesAndExpensesRequest request);
+        Task<Resposta<List<IncomesByCategory>?>> GetIncomesByCategoryReportAsync(GetIncomesByCategoryRequest request);
+        Task<Resposta<List<ExpensesByCategory>?>> GetExpensesByCategoryReportAsync(GetExpensesByCategoryRequest request);
+        Task<Resposta<FinancialSummary?>> GetFinancialSummaryReportAsync(GetFinancialSummaryRequest request);
+    }
+}
